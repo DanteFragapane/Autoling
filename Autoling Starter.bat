ecb75bb626d6bae286b7635392ecf1d771879703
Dante Fragapane <dante.fragapane@gmail.com>
2018-05-04 09:17:20 -0400

    Second

---
+++ Autoling Starter.bat
@@ -0,0 +1,5 @@
+@echo off
+title Autoling Cuddlebot
+echo I am Autoling! I cannot wait to cuddle with you!
+node myBot.js
+pause
---
+++ adminCmd.js
@@ -0,0 +1,9 @@
+exports.admin1 = function() {
+    console.log('in admin 1 command');
+    // your command code here
+}
+
+exports.admin2 = function() {
+    console.log('in admin 2 command');
+    // your command code here
+}
